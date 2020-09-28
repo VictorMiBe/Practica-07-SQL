ffb2235de320b820328aa8bbca2f1cec57a276ac
Victor485 <victoor485@gmail.com>
2020-09-28 17:38:41 -0500

    Se avanzo hasta la 5

---
+++ Primera_Parte/cuarta.sql
@@ -0,0 +1,2 @@
+SELECT nombre, TRUNCATE((precio*0.24),2), TRUNCATE((precio*0.28),2) FROM producto;
+
---
+++ Primera_Parte/quinta.sql
@@ -0,0 +1,3 @@
+SELECT nombre, TRUNCATE((precio*0.24),2) AS euros, TRUNCATE((precio*0.28),2) AS dolares FROM producto;
+
+
