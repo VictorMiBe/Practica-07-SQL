184134849d26a9e08b60c9394c0b7da8117bb7a3
VictorMiBe <59127395+VictorMiBe@users.noreply.github.com>
2020-09-29 17:37:25 -0500

    Update diecisiete.sql

--- Quinta_Parte/diecisiete.sql
+++ Quinta_Parte/diecisiete.sql
@@ -1,7 +1,7 @@
-SELECT *
-FROM producto, fabricante
-WHERE precio >= ALL(SELECT AVG(precio)
+SELECT nombre, nuevatabla.nom_fab
+FROM producto JOIN  (SELECT AVG(precio) media, fabricante.codigo f_cod, fabricante.nombre nom_fab
 					FROM producto, fabricante
 					WHERE producto.codigo_fabricante=fabricante.codigo
-                    GROUP BY fabricante.nombre)
-GROUP BY fabricante.nombre
+                    GROUP BY fabricante.nombre) nuevatabla
+ON producto.codigo_fabricante = nuevatabla.f_cod
+WHERE precio >= nuevatabla.media
